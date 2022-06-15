885b2eea3116a99eceb3d52d54770c2b930027fb
deepak <deepakkumar750206@gmail.com>
2022-06-15 21:15:10 +0530

    fetch image and profile data on DOM

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ const data = [
         name: "Rohan Das",
         age: 21,
         city: 'kolkata',
-        lanuage: "Python",
+        language: "Python",
         framework: 'Django',
-        image: 'https://randomuser.me/api/portraits/men/15.jpg';
+        image: 'https://randomuser.me/api/portraits/men/15.jpg'
     },
     {
         name: "Ravi kumar",
         age: 26,
         city: 'Mumbai',
-        lanuage: "JavaScript",
+        language: "JavaScript",
         framework: 'reactJS',
-        image: 'https://randomuser.me/api/portraits/men/35.jpg';
+        image: 'https://randomuser.me/api/portraits/men/35.jpg'
     },
     {
         name: "Ruchita Verma",
         age: 23,
         city: 'Bhopal',
-        lanuage: "Nodejs",
+        language: "Nodejs",
         framework: 'Express',
-        image: 'https://randomuser.me/api/portraits/women/44.jpg';
+        image: 'https://randomuser.me/api/portraits/women/44.jpg'
     },
     {
         name: "Monty jain",
         age: 27,
         city: 'Faridabad',
-        lanuage: "C",
+        language: "C",
         framework: '.NET',
-        image: 'https://randomuser.me/api/portraits/men/12.jpg';
+        image: 'https://randomuser.me/api/portraits/men/12.jpg'
     },
     {
         name: "Ishita kumari",
         age: 22,
         city: 'Hydrabad',
-        lanuage: "Java",
+        language: "Java",
         framework: 'Spring Boot',
-        image: 'https://randomuser.me/api/portraits/women/28.jpg';
+        image: 'https://randomuser.me/api/portraits/women/28.jpg'
     },
      {
-        name: "naresh",
+        name: "naresh kumar",
         age: 20,
         city: 'Cuttuk',
-        lanuage: "CSS",
+        language: "CSS",
         framework: 'Tailwind',
-        image: 'https://randomuser.me/api/portraits/men/49.jpg';
+        image: 'https://randomuser.me/api/portraits/men/49.jpg'
     }
 ]
 
@@ -75,17 +75,21 @@ next.addEventListener('click',nextCV);
 
 
 function nextCV(){
-    const currentCandidate = candidates.next();
+    const currentCandidate = candidates.next().value;
     let image = document.getElementById('image');
-    image.innerHTML = `<img src= '${currentCandidate.image}'>`;
+    console.log(image);
+    
+    image.innerHTML = `<img src='${currentCandidate.image}'>`;
     let profiles = document.getElementById('profiles');
+    console.log(profiles);
+    
     profiles.innerHTML = `
                         <ul class="list-group">
-                        <li class="list-group-item">${currentCandidate.name}/li>
-                        <li class="list-group-item">${currentCandidate.age}/li>
-                        <li class="list-group-item">${currentCandidate.city}/li>
-                        <li class="list-group-item">${currentCandidate.language}/li>
-                        <li class="list-group-item">${currentCandidate.framework}/li>
+                        <li class="list-group-item">Name:${currentCandidate.name}</li>
+                        <li class="list-group-item">${currentCandidate.age} years old</li>
+                        <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+                        <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+                        <li class="list-group-item">${currentCandidate.framework} framework </li>
                         </ul>
   `
 }
